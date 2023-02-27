9eff84b0c4778fa84a5c9c52b13ad2b606bbf92b
Alex Alves <95006722+DsAlex-lnx@users.noreply.github.com>
2023-02-27 13:08:46 -0300

    Delete environment.development.ts

--- src/environments/environment.development.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production: false, 
-};
-
-export const TIMESTAMPS= '1677324090';
-export const PUBLIC_KEY= '07bf55300e28c133f3ae0ebfa00d9b33';
-export const HASH_KEY= 'a789a840309df3c07e666e77522c793e';
-export const API_PATH = 'https://gateway.marvel.com:443/v1/public/comics?ts=';
-export const GOOGLE_KEY = 'AIzaSyCTbl6_Lbx2NmaInm6XqR1B4OCqzrqk4es'
